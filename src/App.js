3e18bd73618625974d2d161002b657d3001d048f
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-01 17:48:08 +0100

    :arrow_up: Adding Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1>TODO List</h1>
-    </div>
+      <Form />
+    </>
   );
 }
 
---
+++ src/components/Form/Form.js
@@ -0,0 +1,29 @@
+const Form = () => {
+  const sendForm = (event) => {
+    event.preventDefault();
+  };
+
+  const changeValue = () => {};
+
+  const isDisabled = () => {
+    return true;
+  };
+
+  return (
+    <form className="form__todo" onSubmit={sendForm} autoComplete="off">
+      <input
+        className="form__text"
+        type="text"
+        value=""
+        onChange={changeValue}
+        placeholder="Insert the task to do..."
+        required
+      />
+      <button className="form__button" disabled={isDisabled}>
+        Add to list
+      </button>
+    </form>
+  );
+};
+
+export default Form;
