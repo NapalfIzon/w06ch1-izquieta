6c306f8f280a17a2f365b2b10cf0f4b005c40102
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-01 16:59:40 +0100

    :arrow_up: Adding content in App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from "./face01.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>MIND BLOWING - Web in construction.</h1>
+      <h1>TODO List</h1>
     </div>
   );
 }
