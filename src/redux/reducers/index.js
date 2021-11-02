9299a8eb18e3c1befdecb0bac1bedc966f664327
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-02 19:28:53 +0100

    :arrow_up: Adding loadTasks in tasksReducer

--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import tasksReducer from "./taskListReducer";
-
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-});
-
-export default rootReducer;
